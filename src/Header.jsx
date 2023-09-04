a5cf3ad086294b9a9644f58f73349f5e62531db7
Dadabrands <Dadajoshua055@gmail.com>
2023-09-04 15:28:47 +0100

    The Header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { FaSearch } from "react-icons/fa";
+import { BiCart } from "react-icons/bi";
 import "./Header.css";
 
 function Header() {
@@ -10,22 +12,28 @@ function Header() {
         className="w-20 h-auto mx-5 mt-4"
       />
 
-      <div className="header_search">
-        <input className="header_searchInput" type="text" />
-        {/* logo */}
+      <div className="flex flex-1 items-center rounded-full">
+        <input className="h-8 p-2.5 border-none w-full" type="text" />
+        <FaSearch className="p-1 h-8 text-amazonBlack bg-amazonYellow text-2xl" />
       </div>
 
-      <div className="header_nav">
-        <div className="header_option"></div>
-        <span className="header_optionLineOne">Hello User</span>
-        <span className="header_optionLineTwo">Sign In</span>
-        <div className="header_option">
-          <span className="header_optionLineOne">Returns</span>
-          <span className="header_optionLineTwo"> & Orders</span>
+      <div className="flex justify-evenly">
+        <div className="flex flex-col text-white ml-2 mr-2">
+          <span className="text-xs">Hello User</span>
+          <span className="text-sm font-[700]">Sign In</span>
         </div>
-        <div className="header_option">
-          <span className="header_optionLineOne">Your</span>
-          <span className="header_optionLineTwo">Prime</span>
+        <div className="flex flex-col text-white ml-2 mr-2">
+          <span className="text-xs">Returns</span>
+          <span className="text-sm font-[700]"> & Orders</span>
+        </div>
+        <div className="flex flex-col text-white ml-2 mr-2">
+          <span className="text-xs">Your</span>
+          <span className="text-sm font-[700]">Prime</span>
+        </div>
+
+        <div className="">
+          <BiCart />
+          <span className="text-sm font-[700]">0</span>
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
   theme: {
     extend: {
       backgroundColor: {
-        amazonBlack: "#131921", // Replace with your custom color code
+        amazonBlack: "#131921",
+        amazonYellow: "#cd9042",
+      },
+      spacing: {
+        12: "3rem", // Customize the value to match your design
       },
     },
   },
